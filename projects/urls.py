f60ce2d8cbd410f168b6c8ec137130315bb3d763
beltz18 <beltz.18kyodai@gmail.com>
2022-09-13 22:11:05 -0400

    ready for production

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,6 @@ from rest_framework import routers
 from .api           import ProjectViewSet
 
 router = routers.DefaultRouter()
-
 router.register('api/projects', ProjectViewSet, 'projects')
 
 urlpatterns = router.urls
